c38eac12381f051bf762e0e2e7df2cb936956742
khadijehsadeghi <kh.sadeghi95@gmail.com>
2024-04-16 10:36:56 +0330

    repair the search btn

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -51,7 +51,7 @@ const HeroBanner = () => {
                             onChange={( e ) => setQuery( e.target.value )}
                             onKeyUp={searchQueryHandler}
                         />
-                        <button>Search</button>
+                        <button onClick={() => navigate( `/search/${ query }` )}>Search</button>
                     </div>
                 </div>
             </ContentWrapper>
